d2a1c374e64b99ced6629ca24d1cbc97b2255273
Ben Burkert <ben@benburkert.com>
2009-04-08 15:26:32 -0500

    Forgot the user and password attributes.

--- cookbooks/monit/attributes/monit.rb
+++ cookbooks/monit/attributes/monit.rb
@@ -1,3 +1,5 @@
 monit Mash.new unless attribute?('monit')
 
 monit[:application] = @attribute[:applications].keys.first
+monit[:user]        = @attribute[:users].first[:username]
+monit[:password]    = @attribute[:users].first[:password]
